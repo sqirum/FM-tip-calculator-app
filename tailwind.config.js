77dc8a404dfcd1d03085b4f2f20687d6e107da04
sqirum <pauloslh94@gmail.com>
2022-10-19 23:15:58 +0100

    fix tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './dist/index.html',
+    './src/**/*.{html,js}'
+  ],
   theme: {
     extend: {},
   },
